ae16ba30c879b210c8ec8a62c609a43bc21f12fb
Veronica-rhody-219 <ngocdiep219@gmail.com>
2022-11-19 09:37:53 +0700

    feat: update home page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const Book = require("../models/book");
 
-router.get("/", (req, res) => {
-  res.render("index");
+router.get("/", async (req, res) => {
+  let books = [];
+  try {
+    books = await Book.find().sort({createdAt: "desc"}).limit(10).exec();
+  } catch (error) {}
+  res.render("index", {books: books});
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const expressLayouts = require("express-ejs-layouts");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(bodyParser.urlencoded({limit: "10mb", extended: false}));
 
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
